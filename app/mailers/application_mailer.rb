0cb4d946ddcb2331467499bfbad29ee14a98c5a7
vagrant <vagrant@localhost.localdomain>
2020-05-12 14:21:26 +0000

    Add thanks action to action mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,9 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'nonreply@example.com'
   layout 'mailer'
+
+  def thanks(user)
+    @user = user
+    mail to: user.email, subject: "Thank you for signing in!"
+  end
 end
