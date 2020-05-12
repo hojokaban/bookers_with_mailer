d809e7424b0796bfaec6a3cbd0914130f87fe632
vagrant <vagrant@localhost.localdomain>
2020-05-12 15:27:24 +0000

    Add mail test

--- test/mailers/thanks_mailer_test.rb
+++ test/mailers/thanks_mailer_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ThanksMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "thanks" do
+    @user = users(:luffy)
+    mail = ThanksMailer.thanks(@user)
+    assert_equal "Thank you for signing in!", mail.subject
+    assert_equal [@user.email], mail.to
+    assert_equal ["nonreply@example.com"], mail.from
+    assert_match @user.name, mail.body.encoded
+    assert_no_match CGI.escape(@user.email), mail.body.encoded
+  end
 end
