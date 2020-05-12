293c14fed3e5f3d482ba33b5272053c4102fb56b
vagrant <vagrant@localhost.localdomain>
2020-05-12 15:05:42 +0000

    Fix mailer preview

--- test/mailers/previews/mailer_preview.rb
+++
@@ -1,8 +0,0 @@
-class MailerPreview < ActionMailer::Preview
-
-  def thanks
-    user = User.first
-    Mailer.thanks(user)
-  end
-
-end
--- test/mailers/previews/thanks_mailer_preview.rb
+++ test/mailers/previews/thanks_mailer_preview.rb
@@ -1,4 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/thanks_mailer
 class ThanksMailerPreview < ActionMailer::Preview
 
+  def thanks
+    user = User.first
+    Mailer.thanks(user)
+  end
+
 end
