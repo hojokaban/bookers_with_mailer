6fdec927074e82ea819061fe75964904b3b48190
vagrant <vagrant@localhost.localdomain>
2020-05-12 14:58:29 +0000

    Fix mailer.rb

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'nonreply@example.com'
   layout 'mailer'
-
-  def thanks(user)
-    @user = user
-    mail to: user.email, subject: "Thank you for signing in!"
-  end
 end
--- app/mailers/thanks_mailer.rb
+++ app/mailers/thanks_mailer.rb
@@ -1,2 +1,8 @@
 class ThanksMailer < ApplicationMailer
+
+  def thanks(user)
+    @user = user
+    mail to: @user.email, subject: "Thank you for signing in!"
+  end
+
 end
